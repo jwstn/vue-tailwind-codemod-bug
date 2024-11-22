8fce14afa7c01644772db3bd7ae96d11d00cc948
DevNewbieJW <julian.weusten@fondof.de>
2024-11-22 12:58:52 +0100

    inital commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import Input from './components/Input.vue'
+
+function foobar() {
+    console.log('blur!')
+}
+
+</script>
+
+<template>
+    <div class="h-dvh w-dvw flex items-center justify-center bg-slate-700">
+        <h1>Test</h1>
+        <Input @blur=foobar />
+    </div>
+</template>
---
+++ src/components/Input.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+
+const emits = defineEmits<{
+    (e: 'blur'): void
+}>()
+
+</script>
+<template>
+    <input class="px-4 py-2 bg-white rounded text-base text-lack" placeholder="type here..." @blur="emits('blur')" />
+</template>
